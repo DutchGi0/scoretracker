ec77f6ef51733876736813de0ebaa92bcd8b2eb1
Gio <113716@mborijnland.nl>
2022-10-19 11:51:53 +0200

    teams

--- pages/teams.php
+++ pages/teams.php
@@ -1,3 +1,11 @@
+<!-- Get teams DB -->
+<?php
+$sql = 'SELECT * FROM team INNER JOIN user ON team.userid = user.id';
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$teams = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,7 +20,35 @@
     <div class="container-fluid">
         <h1 class='title'>Teams</h1>
         <br>
-        Show teams
+        <!-- Show teams in table -->
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th>Team</th>
+                    <th>Goals</th>
+                    <th>Assists</th>
+                    <th>View team</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($teams as $team) {
+                    $id = $team['ID'];
+                    $goals = $team['goal'];
+                    $assists = $team['assist'];
+                    // calculate team players
+                    ?>
+                <tr>
+                    <td><?php echo $team['tm_name']; ?></td>
+                    <td><?php echo $goals; ?></td>
+                    <td><?php echo $assists; ?></td>
+                    <td><?php echo "<span class='right uppercase'> <a class='btn btn-primary' href='index.php?page=team&id=" .
+                            $team['id'] .
+                            "'>View team</a></span>"; ?>
+                </tr>
+                <?php
+                } ?>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
