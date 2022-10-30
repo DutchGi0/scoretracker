e495bc779db613fd7d5c966f8ebd3c7e5b5851f4
Gio <113716@mborijnland.nl>
2022-10-30 02:41:42 +0200

    Teams updated

--- pages/admin.php
+++ pages/admin.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['ID'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="css/styles.css">
-    <script src="js/jquery-3.6.0.min.js" defer></script>
+    <script src="js/jquery-3.6.1.min.js" defer></script>
     <script src="bootstrap/js/bootstrap.min.js" defer></script>
     <title>Stat Tracker - Admin CP</title>
 </head>
@@ -23,7 +23,7 @@ if (!isset($_SESSION['ID'])) {
         <h1 class='title'>Admin CP</h1>
         <br>
         <!-- The movie overview, you can edit and delete movies from this overview -->
-        <h2 class="title">Users</h2>
+        <h2 class="h2-title">Users</h2>
     <table class="table table-dark table-striped">
         <thead>
             <tr>
@@ -35,7 +35,7 @@ if (!isset($_SESSION['ID'])) {
         </thead>
         <tbody>
             <?php
-            $sql = 'SELECT * FROM user';
+            $sql = 'SELECT * FROM user INNER JOIN team ON user.teamid = team.id';
             $stmt = $db->prepare($sql);
             $stmt->execute([]);
             $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -43,8 +43,8 @@ if (!isset($_SESSION['ID'])) {
                 $id = $user['id'];
                 echo '<tr>';
                 echo '<td>' . $user['name'] . '</td>';
-                echo '<td>' . $user['team'] . '</td>';
-                echo "<td><a style='text-decoration: none;' href='index.php?page=movies_edit&id=" .
+                echo '<td>' . $user['tm_name'] . '</td>';
+                echo "<td><a style='text-decoration: none;' href='index.php?page=user_edit&id=" .
                     $user['id'] .
                     "'>&#9989;</a></td>";
                 echo "<td><a onclick='javascript:confirmationDelete($(this));return false;' style='text-decoration: none;'  href='index.php?page=movies_delete&id=" .
@@ -54,6 +54,37 @@ if (!isset($_SESSION['ID'])) {
             ?>
             </tbody>
         </table>
+
+        <h2 class="h2-title">Teams</h2>
+    <table class="table table-dark table-striped">
+        <thead>
+            <tr>
+                <th>Team</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $sql = 'SELECT * FROM team';
+            $stmt = $db->prepare($sql);
+            $stmt->execute([]);
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($users as $user) {
+                $id = $user['id'];
+                echo '<tr>';
+                echo '<td>' . $user['tm_name'] . '</td>';
+                echo "<td><a style='text-decoration: none;' href='index.php?page=team_edit&id=" .
+                    $user['id'] .
+                    "'>&#9989;</a></td>";
+                echo "<td><a onclick='javascript:confirmationDelete($(this));return false;' style='text-decoration: none;'  href='index.php?page=movies_delete&id=" .
+                    $user['id'] .
+                    "'>&#10062;</a></td>";
+            }
+            ?>
+            </tbody>
+        </table>
+        <a class="btn btn-dark right" href="#" role="button">Add</a>
 </div>
 <script>
     function confirmationDelete(anchor){
--- pages/team.php
+++ pages/team.php
@@ -1,12 +1,51 @@
+<?php 
+$sql = 'SELECT * FROM user INNER JOIN team ON user.teamid = team.id';
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$teams = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+
+$sql = 'SELECT * FROM team WHERE id = :id';
+$stmt = $db->prepare($sql);
+$stmt->execute(['id' => $_GET['id']]);
+$team = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$name = $team['tm_name'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stat Tracker <?php echo $team_nm?></title>
+    <title>Stat Tracker - <?php echo $name;
+    ?></title>
 </head>
 <body>
-    
+    <div class="container">
+        <h1 class='title'><?php echo $name ?></h1>
+
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th>Player</th>
+                    <th>Goals</th>
+                    <th>Assists</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($teams as $team) {
+                    $id = $team['id'];
+                    ?>
+                <tr>
+                    <td><?php echo $team['name']; ?></td>
+                    <td><?php echo $team['goal']; ?></td>
+                    <td><?php echo $team['assist']; ?></td>
+                </tr>
+                <?php
+                } ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
